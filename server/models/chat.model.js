c6a7fd99d5b2634a5166b70850874ab507c3f1bb
Youssef-Kobi <youssefkobi@gmail.com>
2022-09-23 16:19:26 +0100

    db:Conversation Schema

--- server/models/chat.model.js
+++ server/models/chat.model.js
@@ -1,62 +1,15 @@
-{
-  // _id:
-  chatId: 1234,
-  sender_id:
-  receiver_id:
-  subject:
-  updated_at:
-  message: {
-          message:
-          messageId: 1,
-          author_id:
-          attatchments: [x,y,z],
-          read:
-          created_at:
-          }
-},
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
-const UserSchema = new mongoose.Schema(
+const MessageSchema = new mongoose.Schema(
   {
-    sender_id: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        'Please fill a valid email address',
-      ],
-    },
-    receiver_id: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    subject: {
-      type: String,
-      required: true,
-    },
-    updated_at: {
-      type: Object,
-      // required: true,
-      default: {},
-    },
-    message: {
       message: {
         type: String,
-        unique: true,
-        required: true,
-      },
-      messageId:{
-        type: String,
-        unique: true,
         required: true,
       },
       author_id:{
-        type: String,
-        unique: true,
-        required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
       },
       attachments:{
         type: String,
@@ -64,27 +17,38 @@ const UserSchema = new mongoose.Schema(
         required: true,
       },
       read:{
-        type: String,
-        unique: true,
-        required: true,
-      }
+        type: Boolean,
+        default : false,
+      },
     },
+    {
+      timestamps: true,
+    }
+  )
+const ConversationSchema = new mongoose.Schema(
+  {
+    sender_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    receiver_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    },
+    message : {
+      type: MessageSchema,
+    }
   },
   {
     timestamps: true,
   }
 );
 
-UserSchema.pre('save', function (next) {
-  if (!this.isModified('password')) return next();
+const Conversation = mongoose.model('User', ConversationSchema);
 
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(this.password, salt);
-
-  this.password = hash;
-
-  next();
-});
-const User = mongoose.model('User', UserSchema);
-
-export default User;
+export default Conversation;
